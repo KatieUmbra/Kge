746f4f35c44e1cda642d415523ca4fc635328fb3
106122241+KatieUmbra@users.noreply.github.com <Katherine@kaytea.dev>
2023-11-24 14:06:16 -0600

    .

--- libs/clean_libs.sh
+++ libs/clean_libs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rm -rf ./include/GLAD ./include/GLFW ./include/GLM glad/include glad/src install.lock ./include/STB/stb_image.h
+rm -rf ./include/GLAD ./include/GLFW ./include/GLM glad/include glad/src install.lock ./include/STB/stb_image.h ./include/KHR
